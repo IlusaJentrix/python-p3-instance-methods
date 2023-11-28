69a75e57135aeacb70200833b18d98b8946fd0ec
Jentrix <jentrix.ilusa@student.moringaschool.com>
2023-11-28 14:42:49 +0300

    completed assignment

--- lib/dog.py
+++ lib/dog.py
@@ -1,7 +1,31 @@
 #!/usr/bin/env python3
+import types
+
 
 class Dog:
-    # Class body goes here
+    def bark(self):
+        print("Woof!")
 
-    #Instance method definition
-    pass
+
+fido = Dog()
+
+# Call the bark method on the fido instance
+fido.bark()
+# Output: Woof!
+
+snoopy = Dog()
+
+# Call the bark method on the snoopy instance
+snoopy.bark()
+# Output: Woof!
+
+class Dog:
+    def sit(self):
+        print("The dog is sitting.")
+
+# Create another instance of the Dog class
+buddy = Dog()
+
+# Call the sit method on the buddy instance
+buddy.sit()
+# Output: The dog is sitting.
--- lib/person.py
+++ lib/person.py
@@ -1,7 +1,22 @@
 #!/usr/bin/env python3
 
 class Person:
-    # Class body goes here
+    def talk(self):
+        print("Hello World!")
 
-    #Instance method definition
-    pass
+# Create an instance of the Person class
+ilusa = Person()
+
+# Call the talk method on the ilusa instance
+ilusa.talk()
+
+class Person:
+    def walk(self):
+        print("The person is working.")
+
+jenty = Person()
+
+jenty.walk()
+
+
+    
